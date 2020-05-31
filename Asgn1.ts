aa4d3363d10200a3e4606effa0e43164fbd0f96a
jack-yifei-wang <53378207+jack-yifei-wang@users.noreply.github.com>
2020-05-30 23:26:25 -0700

    Create Asgn1.ts

---
+++ Asgn1.ts
@@ -0,0 +1,46 @@
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  Input,
+  Output,
+  EventEmitter
+} from "@angular/core";
+
+/** Prompt to reorient at the beginning of a new quarter. */
+@Component({
+  selector: "app-new-quarter",
+  templateUrl: "./new-quarter.component.html",
+  styleUrls: ["./new-quarter.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class NewQuarterComponent implements OnInit {
+  // --------------- INPUTS AND OUTPUTS ------------------
+
+  /** Events for initiating quarterly reorientation. */
+  @Output() reorient: EventEmitter<void> = new EventEmitter<void>();
+
+  // --------------- LOCAL UI STATE ----------------------
+  //@Input() message: ChangeDetectionStrategy;
+  header_text:string = "It's a new quarter!";
+  header_text_2: string = "Time to set some new goals.";
+  button_text:string = "Get started";
+
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    
+  }
+
+  // --------------- DATA BINDING FUNCTIONS --------------
+  getstarted(){
+    console.log("clicked_button");  //when button clicked, you can see messages in the console. 
+  }
+
+  // --------------- EVENT BINDING FUNCTIONS -------------
+
+  // --------------- OTHER -------------------------------
+}
